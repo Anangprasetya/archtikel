57f6de4a4b06398c9e016ec9adabe95d89cfdaaf
Anang <tyoandini@gmail.com>
2022-12-06 20:05:52 +0700

    ubah view di controller header & footer

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -8,9 +8,11 @@ class Home extends Controller
 		if (isset($_SESSION["user_login"])) {
 			$this->view('layout/header');
 			$this->view('myhomelogin');
-			$this->view('layout/header');
+			$this->view('layout/footer');
 		} else {
+			$this->view('layout/header');
 			$this->view('myhome');
+			$this->view('layout/footer');
 		}
 	}
 }
