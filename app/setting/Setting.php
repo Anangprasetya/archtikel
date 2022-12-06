7a56c14389eb690fc189ac7a6e5f0b002c2d25dd
Anang <tyoandini@gmail.com>
2022-12-06 14:34:54 +0700

    menambah file core & default controller view

---
+++ app/setting/Setting.php
@@ -0,0 +1,45 @@
+<?php
+
+// ======================
+// URL
+// ======================
+define('BASEURL', 'http://localhost/pwdTugas/repo/archtikel/public/');
+
+
+// ======================
+// DATABASE
+// ======================
+
+/*
+	HOST, USER, PASS, NAME
+	Disesuaikan dengan database kalian masing masing
+
+	contoh :
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'root');
+	define('DB_PASS', 'root12345');
+	define('DB_NAME', 'mydatabase');
+
+*/
+define('DB_HOST', '');
+define('DB_USER', '');
+define('DB_PASS', '');
+define('DB_NAME', '');
+
+
+
+
+
+
+
+// ======================
+// DEFAULT CONTROLLER
+// ======================
+define('Controller', 'Home');
+
+
+
+// ======================
+// DEFAULT METHOD
+// ======================
+define('Method', 'index');
