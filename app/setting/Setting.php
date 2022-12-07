ff523df20a688d414ceeea9a765804540c773881
Anang <tyoandini@gmail.com>
2022-12-07 12:11:41 +0700

    menambah setting di Setting.php

--- app/setting/Setting.php
+++ app/setting/Setting.php
@@ -3,7 +3,8 @@
 // ======================
 // URL
 // ======================
-define('BASEURL', 'http://localhost/archtikel/public/');
+// define('BASEURL', 'http://localhost/archtikel/public/');
+define('BASEURL', 'http://localhost/pwdTugas/repo/archtikel/public/');
 
 
 // ======================
@@ -21,12 +22,16 @@ define('BASEURL', 'http://localhost/archtikel/public/');
 	define('DB_NAME', 'mydatabase');
 
 */
+/*
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'artikel');
-
-
+*/
+define('DB_HOST', 'localhost');
+define('DB_USER', 'admin');
+define('DB_PASS', 'password');
+define('DB_NAME', 'test_artikel');
 
 
 
